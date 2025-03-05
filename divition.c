4dde0d8c3cdfc951866211850f0db51b27cd4ec7
Aditya Vishwakarma <aditya1831v@gmail.com>
2025-03-05 11:28:35 +0530

    Create divition.c

---
+++ divition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter the first number:");
+  scanf("%d",&a);
+ printf("Enter the secound number:");
+  scanf("%d",&b);
+  c=a/b;
+  printf("divition is the %d/%d=%d",a,b,c);
+  return 0;
+}
+  
